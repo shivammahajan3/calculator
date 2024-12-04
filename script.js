d484282702f1f67a3d8ab13342916804214b0251
shivammahajan3 <shimahajan@bestpeers.in>
2024-12-04 18:55:38 +0530

    adding javascript functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+let string = "";
+let buttons = document.querySelectorAll(".buttons");
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener("click", (event)=>{
+        if(event.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(event.target.innerHTML == 'AC'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if(event.target.innerHTML == 'DEL'){
+            string = string.slice(0, string.length-1);
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(event.target)
+            string = string + event.target.innerHTML
+            document.querySelector('input').value = string;
+        }
+    })
+})
