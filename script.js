703fb38e8d2ca925b463f8097dbb8b39ebc1f74a
shivammahajan3 <shimahajan@bestpeers.in>
2024-12-05 11:40:18 +0530

    fixing percentage functionality

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ let buttons = document.querySelectorAll(".buttons");
 Array.from(buttons).forEach((button)=>{
     button.addEventListener("click", (event)=>{
         if(event.target.innerHTML == '='){
-            string = eval(string);
-            document.querySelector('input').value = string;
+            if(string.includes("%")){
+                string = string.replace("%", "*");
+                string = eval(string);
+                let num = parseInt(string);
+                num = num / 100;
+                string = num.toString();
+                document.querySelector('input').value = string;
+            }else{
+                string = eval(string);
+                document.querySelector('input').value = string;
+            }
         }
         else if(event.target.innerHTML == 'AC'){
             string = "";
             document.querySelector('input').value = string;
         }
+        
         else if(event.target.innerHTML == 'DEL'){
             string = string.slice(0, string.length-1);
             document.querySelector('input').value = string;
